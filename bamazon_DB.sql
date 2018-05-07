60e2b86b3958590a882499291a745d9f43cc994f
@Donskoy <diman@rocketmail.com>
2018-05-07 13:58:04 -0500

    test inserts into db

--- bamazon_DB.sql
+++ bamazon_DB.sql
@@ -6,11 +6,11 @@ USE bamazon_DB;
 CREATE TABLE inventory (
     id INTEGER(11) AUTO_INCREMENT NOT NULL,
     product_name VARCHAR(100) NOT NULL,
-    department_name VARCHAR(20) NOT NULL,
-    price FLOAT(100) DEFAULT 0,
-    stock_quantity INTEGER(10) DEFAULT 0,
+    department_name VARCHAR(45) NOT NULL,
+    price DECIMAL(100,2) DEFAULT 0,
+    stock_quantity INTEGER(100) DEFAULT 0,
     PRIMARY KEY (id)
 );
 
--- INSERT INTO inventory (flavor, price, quantity)
--- VALUES ("vanilla", 2.50, 100), ("chocolate", 3.10, 120), ("strawberry", 3.25, 75);
+INSERT INTO inventory (product_name, department_name, price, stock_quantity)
+VALUES ("vanilla", "ice cream", 2.50, 100), ("chocolate", "candy", 3.10, 120), ("strawberry", "fruit", 3.25, 75);
